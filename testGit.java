c60ccd5084cec7e91e4c4b12035430140145c603
Héctor <hectorgarcia05.hgg@gmail.com>
2025-03-28 12:36:09 +0100

    Proyecto que identifica la persona que dice hola a Carmelo

--- testGit.java
+++ testGit.java
@@ -1,3 +1,21 @@
-pubic static void main(String[] args){
-	System.out.println("Hola Carmelo");
+class Persona{
+
+private int edad;
+private String nombre;
+
+public Persona(int edad, String nombre){
+
+	this.edad = edad;
+	this.nombre = nombre;
+}
+public String getNombre(){
+	return nombre;
+}
+public int getEdad(){
+	return edad;
+}
+}
+public static void main(String[] args){
+	Persona p1 = new Persona(19,"Samuel");
+	System.out.println("La persona : "+p1.getNombre()+"con edad: "+p1.getEdad()+"Dice: Hola Carmelo");
 }
